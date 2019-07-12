462e16a3b61074274808decaddf62edb91cf4a61
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2019-07-12 08:48:12 -0400

    updated help message

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ client.on('ready', () => {
 client.on('message', (message) => {
 	if (message.content == '$help') {
 		let m =
-			'fsb-ticker. Developed by BuffMan \n\n Example commands: \n `$avgo`\n `$aapl w`\n `$tsla d rsi macd`\n\n' +
-			'Contact Buffman for more info and any feature requests :)';
+			'fsb-ticker. Developed by BuffMan \n\n Example commands: \n `$avgo`\n `$aapl w`\n `$tsla d rsi macd`\n `$spy line`\n `$/es`\n `$.btc`\n `$usd/jpy w`\n `$sectors ytd`\n\n' +
+			'**Currently Sheduled Features**\n' +
+			'- Add support for BollingerBands and other Overlays\n\n' +
+			'_Contact Buffman for more info and any feature requests_\n';
 		message.channel.send(m);
 	} else if (message.content.startsWith('$.')) {
 		console.log('CRYPTO');
