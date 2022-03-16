489340ed7e973ab0a900eb4b57532085ff5f901b
mafuchs <mikeafruchs@gmail.com>
2022-03-15 20:49:02 -0500

    add sq api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
 import Discord from 'discord.js';
 import { connect } from 'mongoose';
 import NodeCache from 'node-cache';
-
+import express from 'express';
+import got from 'got';
 import { commandList } from './commandlist';
 
 require('dotenv').config();
+
+const app = express();
+
+// use the express-static middleware
+app.use(express.static('public'));
+
+app.get('/latest', (req, res) => {
+  got(`${process.env.SQ_URI}/latest?key=${process.env.SQ_KEY}`).json().then((value) => {
+    res.json(value);
+  });
+});
+
+app.get('/symbol/:ticker', (req, res) => {
+  got(`${process.env.SQ_URI}/symbol/${req.params.ticker}?key=${process.env.SQ_KEY}`).json().then((value) => {
+    res.json(value);
+  });
+});
+
+app.get('/screen/:which', (req, res) => {
+  got(`${process.env.SQ_URI}/symbol/${req.params.which}?key=${process.env.SQ_KEY}`).json().then((value) => {
+    res.json(value);
+  });
+});
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000,
+  () => console.log('Server is running...'));
 
 const client = new Discord.Client();
 client.on('ready', () => {
